cd0f4933a4592ea910f4933f4a79ef84f70a1596
Aleksandr Malomed <malomed_az@itstep.edu.az>
2019-07-31 19:35:33 +0400

    commit 42

--- HttpListenerDemo/Program.cs
+++ HttpListenerDemo/Program.cs
@@ -7,23 +7,63 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HttpListenerDemo {
+
+    class Seen {
+        public int Id { get; set; }
+        public virtual Poteryashka Who { get; set; }
+        public string Where { get; set; }
+        public DateTime When { get; set; }
+    }
+    class Poteryashka {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string AdditionalInfo { get; set; }
+        public DateTime? Lost { get; set; }
+        public DateTime? Found { get; set; }
+        public string Phone { get; set; }
+        public int Age { get; set; }
+        public bool IsFound { get; set; }
+        public virtual ICollection<Seen> Seen { get; set; }
+    }
+
+
+
     class Program {
         static void Main(string[] args) {
-            var server = new HttpListener();
-            server.Prefixes.Add("http://localhost:27001/");
-            server.Start();
 
-            while(true) {
-                var context = server.GetContext();
-                var name = context.Request.QueryString["name"];
-                var lang = context.Request.RawUrl.Split('/')[1];
-                //var stream = context.Response.OutputStream;
-                //var writer = new StreamWriter(stream);
-                //writer.WriteLine($"<h1 style='color:red'>{(lang == "en" ? "Hello" : "Salam")}, <s>{name}</s></h1>");
-                //writer.Close();
-                context.Response.StatusCode = 515;
-                context.Response.Close();
+
+
+
+            var props = new[] {
+                "Hair", "Nose", "Eyes"
+            };
+            
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine($"{Faker.ArrayFaker.SelectFrom(props)} {Faker.EnumFaker.SelectFrom<ConsoleColor>()}");
+
+
+                // Console.WriteLine($"{Faker.LocationFaker.Country()} {Faker.LocationFaker.City()} {Faker.LocationFaker.Street()}");
             }
+
+
+
+
+            //var server = new HttpListener();
+            //server.Prefixes.Add("http://localhost:27001/");
+            //server.Start();
+
+            //while(true) {
+            //    var context = server.GetContext();
+            //    var name = context.Request.QueryString["name"];
+            //    var lang = context.Request.RawUrl.Split('/')[1];
+            //    //var stream = context.Response.OutputStream;
+            //    //var writer = new StreamWriter(stream);
+            //    //writer.WriteLine($"<h1 style='color:red'>{(lang == "en" ? "Hello" : "Salam")}, <s>{name}</s></h1>");
+            //    //writer.Close();
+            //    context.Response.StatusCode = 515;
+            //    context.Response.Close();
+            //}
         }
     }
 }
